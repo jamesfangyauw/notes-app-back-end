d45cfd7cb2e82f241deb2ccab18467a1f039346a
James <jamesfangyauw@gmail.com>
2022-07-20 15:31:07 +0800

    initial commit

--- src/handler.js
+++ src/handler.js
@@ -110,7 +110,8 @@ const deleteNoteByIdHandler = (request, h) => {
   response.code(404);
   return response;
 };
-console.log ("test test")
+console.log('test test');
 module.exports = {
+  // eslint-disable-next-line max-len
   addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler,
 };
